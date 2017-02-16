d9277e8b59021b72e6c86ab98569ec2033d07857
Briano Santos <brianosantos@gmail.com>
2017-02-16 09:33:55 -0800

    Initial Commit

---
+++ PageTheScroll_Self/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PageTheScroll_Self
+//
+//  Created by Briano Santos on 2/16/17.
+//  Copyright © 2017 FairSkiesTechnologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
